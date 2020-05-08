eb976e8c5b79dcac4660b558607b9628bec93cfa
elsaardianti <61940816+elsaardianti@users.noreply.github.com>
2020-05-08 17:45:33 +0700

    Add files via upload

---
+++ Epson.cs
@@ -0,0 +1,13 @@
+ using System;
+
+public class Epson : PrinterWindows
+{
+    public override void Show()
+    {
+      Console.WriteLine("Epson display dimension : 10*11");  
+    }
+    public override void Print()
+    {
+        Console.WriteLine("Epson Printer is Printing ...");
+    }
+}
---
+++ PrinterWindows.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class PrinterWindows
+{
+   public virtual void Show()
+    {
+      Console.WriteLine("Sony display dimension : 10*11");  
+    }
+    public virtual void Print()
+    {
+        Console.WriteLine("Sony Printer is Printing ...");
+    }
+}
